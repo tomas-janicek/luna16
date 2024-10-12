66aeddfa94f0d6ffcacb49ce457c37aa9395ed22
Tomas <tomasjanicek221@gmail.com>
2024-10-12 20:08:06 +0000

    fix: training scripts

--- scripts/download_files.sh
+++ scripts/download_files.sh
@@ -4,14 +4,14 @@ set -e
 
 mkdir -p data_downloaded/ct_scan_subsets/
 
-wget https://zenodo.org/records/3723295/files/subset0.zip
+wget -P data_downloaded/ https://zenodo.org/records/3723295/files/subset0.zip
 unzip data_downloaded/subset0.zip -d data_downloaded/ct_scan_subsets/
 rm data_downloaded/subset0.zip
 
-wget https://zenodo.org/records/3723295/files/subset1.zip
+wget -P data_downloaded/ https://zenodo.org/records/3723295/files/subset1.zip
 unzip data_downloaded/subset1.zip -d data_downloaded/ct_scan_subsets/
 rm data_downloaded/subset1.zip
 
-wget https://zenodo.org/records/3723295/files/subset2.zip
+wget -P data_downloaded/ https://zenodo.org/records/3723295/files/subset2.zip
 unzip data_downloaded/subset2.zip -d data_downloaded/ct_scan_subsets/
 rm data_downloaded/subset2.zip
--- scripts/run-pod-training.sh
+++ scripts/run-pod-training.sh
@@ -4,5 +4,5 @@ set -e
 
 export PYTHONPATH=.
 
-uv run python luna16/cli.py train_luna_classification --epochs=1 \
+uv run python luna16/cli.py train_luna_classification --epochs=10 \
                                                       --batch-size=64

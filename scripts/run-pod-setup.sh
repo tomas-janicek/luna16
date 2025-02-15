301599a5156edb1acb150b686bee081433d93a5b
Tomas Janicek <tomas.janicek@redampsecurity.com>
2025-02-15 13:56:54 +0100

    fix: correctly clone .env for runpod

--- scripts/run-pod-setup.sh
+++ scripts/run-pod-setup.sh
@@ -14,7 +14,7 @@ git config --global user.name Tomas Janicek
 apt-get update
 apt-get install unzip
 
-echo -e >> .env
+cp .env.example .env
 echo -e "NUM_WORKERS=64" >> .env
 echo -e "CACHE_DIR=/workspace/cache" >> .env
 echo -e "DATA_DOWNLOADED_DIR=/workspace/data_downloaded" >> .env

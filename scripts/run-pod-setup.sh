28e30e14440e137f9a809216746d3fd34c5027a0
Tomas <tomasjanicek221@gmail.com>
2025-02-15 19:05:52 +0000

    fix: run pod setup

--- scripts/run-pod-setup.sh
+++ scripts/run-pod-setup.sh
@@ -12,9 +12,10 @@ git config --global user.email "tomasjanicek221@gmail.com"
 git config --global user.name Tomas Janicek
 
 apt-get update
-apt-get install unzip
+apt-get install unzip screen
+
 
 cp .env.example .env
-echo -e "NUM_WORKERS=64" >> .env
+echo -e "NUM_WORKERS=16" >> .env
 echo -e "CACHE_DIR=/workspace/cache" >> .env
-echo -e "DATA_DOWNLOADED_DIR=/workspace/data_downloaded" >> .env
+echo -e "DATA_DOWNLOADED_DIR=/workspace" >> .env
